4ae91844c3634026f8a9b0ccd3d7d17b0e0b5f3f
Nicholas Devenish <ndevenish@gmail.com>
2025-10-27 12:49:36 +0000

    Work using rzmq

--- src/bin/fauxdin.rs
+++ src/bin/fauxdin.rs
@@ -1,8 +1,50 @@
 use anyhow::Result;
-use epicars::client::Watcher;
+use epicars::{ServerBuilder, client::Watcher};
+use rzmq::{Msg, ZmqError};
 use tracing::info;
 use tracing_subscriber::EnvFilter;
-use zeromq::Socket;
+
+async fn maybe_recv_multipart(
+    t: &mut Option<CancelSafeSocket>,
+) -> Option<Result<Vec<rzmq::Msg>, rzmq::ZmqError>> {
+    match t {
+        Some(socket) => Some(socket.recv_multipart().await),
+        None => None,
+    }
+}
+
+/// Wrap an [`rzmq::Socket`] in a cancel-safe way
+struct CancelSafeSocket {
+    socket: rzmq::Socket,
+    messages: Vec<Msg>,
+}
+
+impl CancelSafeSocket {
+    pub fn new(socket: rzmq::Socket) -> Self {
+        Self {
+            socket,
+            messages: Vec::new(),
+        }
+    }
+    /// Receive a multi-part message. Cancel-safe.
+    pub async fn recv_multipart(&mut self) -> Result<Vec<Msg>, ZmqError> {
+        loop {
+            let msg = match self.socket.recv().await {
+                Ok(m) => m,
+                Err(e) => return Err(e),
+            };
+            let is_last_message = !msg.is_more();
+            self.messages.push(msg);
+            if is_last_message {
+                break;
+            }
+        }
+        Ok(std::mem::take(&mut self.messages))
+    }
+    pub async fn close(&mut self) -> Result<(), ZmqError> {
+        self.socket.close().await
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -25,28 +67,51 @@ async fn main() -> Result<()> {
         .unwrap()
         .watch();
 
+    let ctx_in = rzmq::Context::new().unwrap();
+    let ctx_out = rzmq::Context::new().unwrap();
+
     // Create the pipe out, for others to connect to
-    let mut out = zeromq::PushSocket::new();
     let push_endpoint = "tcp://0.0.0.0:9999";
     info!("Creating output PUSH socket {push_endpoint}");
-    out.bind(push_endpoint).await?;
+    let sock_out = ctx_out.socket(rzmq::SocketType::Push)?;
 
     // Create the input pipe, that connects to the detector
-    let mut zmq_in = zeromq::PullSocket::new();
-    info!(
-        "Connecting PULL socket to {}",
-        target.borrow_and_update().unwrap()
-    );
-    zmq_in.connect(&target.borrow_and_update().unwrap()).await?;
+    let mut sock_in = Some({
+        let current_target = target.borrow_and_update().unwrap();
+        info!("Connecting PULL socket to {current_target}");
+        let socket = ctx_in.socket(rzmq::SocketType::Pull)?;
+        socket.connect(&current_target).await?;
+        CancelSafeSocket::new(socket)
+    });
+
+    let _server = ServerBuilder::new(library).start().await.unwrap();
+
     loop {
         tokio::select! {
-            _ = target.changed() => {
+            Ok(_) = target.changed() => {
+                // The user changed the connection endpoint via our PV server
                 let endpoint = target.borrow_and_update().unwrap();
-                info!("Connection target changed to {endpoint}");
-                zmq_in.connect(&endpoint).await?;
+                if let Some(mut sock_to_close) = sock_in.take() {
+                    tokio::task::spawn(async move {let _ = sock_to_close.close().await;});
+                }
+                if endpoint.is_empty() {
+                    info!("Connection endpoint cleared, suspending connection");
+                    continue;
+                }
+
+                info!("Connection target changed to {endpoint}, making new connection");
+                sock_in = Some({
+                    let socket = ctx_in.socket(rzmq::SocketType::Pull)?;
+                    socket.connect(&endpoint).await?;
+                    CancelSafeSocket::new(socket)
+                });
                 continue;
             },
-
+            Some(messages) = maybe_recv_multipart(&mut sock_in) => {
+                let msg = messages.unwrap();
+                println!("Received: {msg:?}");
+                sock_out.send_multipart(msg).await.unwrap();
+            }
         }
     }
 }
